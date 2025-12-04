96ffc613d32366f2d4b36b6bbf169807d59d034e
RamKumar <ramkumar@itworks1ders.in>
2025-12-04 12:53:02 +0530

    GetmetalayoutByid

---
+++ TaskEngineAPI/Helpers/ReaderExtension.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SqlClient;
+
+namespace TaskEngineAPI.Helpers
+{
+    
+
+        public  static class ReaderExtension
+        {
+            public static string GetSafeString(this SqlDataReader r, string col) =>
+                r[col] == DBNull.Value ? "" : r[col].ToString();
+
+            public static int GetSafeInt(this SqlDataReader r, string col) =>
+                r[col] == DBNull.Value ? 0 : Convert.ToInt32(r[col]);
+
+            public static bool GetSafeBool(this SqlDataReader r, string col) =>
+                r[col] != DBNull.Value && Convert.ToBoolean(r[col]);
+
+            public static DateTime? GetSafeDate(this SqlDataReader r, string col) =>
+                r[col] == DBNull.Value ? null : Convert.ToDateTime(r[col]);
+        }
+
+  
+
+
+
+}
