cc48ea18ffcf85dc5e37e0f24de9c15cdfbb41f4
Keerthana Devi <Keerthana Devi@KEERTHANADEVI-L>
2025-12-17 12:27:57 +0530

    modified GetProcessMetadetailbyid

--- TaskEngineAPI/Interfaces/ITaskMasterService.cs
+++ TaskEngineAPI/Interfaces/ITaskMasterService.cs
@@ -7,7 +7,7 @@ namespace TaskEngineAPI.Interfaces
     public interface ITaskMasterService
     {
         Task<int> InsertTaskMasterAsync(TaskMasterDTO model, int tenantId, string username);
-        Task<string> GetAllProcessmetaAsync(int cTenantID, int metaid);
+        Task<string> GetAllProcessmetaAsync(int cTenantID, int processid);
         Task<string> GetAllProcessmetadetailAsync(int cTenantID, int metaid);
         Task<string> Getdepartmentroleposition(int cTenantID, string table);
         Task<string> Getprocessengineprivilege(int cTenantID, string value, string cprivilege);
