5f5c8378bb27ed0ec3a9b49862356f0a2d99f5dd
RamKumar <ramkumar@itworks1ders.in>
2026-01-28 09:26:06 +0530

    InsertAnalyticalhubAsync

--- TaskEngineAPI/Services/AnalyticalService.cs
+++ TaskEngineAPI/Services/AnalyticalService.cs
@@ -8,6 +8,7 @@ using TaskEngineAPI.Helpers;
 using Newtonsoft.Json;
 using System.Net.Mail;
 using Microsoft.Extensions.Options;
+using System.Collections.Generic;
 namespace TaskEngineAPI.Services
 {
     public class AnalyticalService: IAnalyticalService
@@ -38,9 +39,8 @@ namespace TaskEngineAPI.Services
                         string queryMaster = @"
                     INSERT INTO [tbl_analytical_workspace] (ctenant_id,canalyticalname,canalyticalDescription,canalyticalprompt,capi_method,
                     capi_url,capi_params,capi_headers,cbody,nis_active,ccreated_by,lcreated_date,cmodified_by,lmodified_date) 
-                     VALUES (@ctenant_id,@canalyticalname,@canalyticalDescription,@canalyticalprompt,@capi_method,@capi_url,@capi_params 
-                     @capi_headers,@cbody,@nis_active,@ccreated_by,@lcreated_date,@cmodified_by,@lmodified_date );SELECT SCOPE_IDENTITY();";
-
+                     VALUES (@ctenant_id,@canalyticalname,@canalyticalDescription,@canalyticalprompt,@capi_method,@capi_url,@capi_params, 
+                     @capi_headers,@cbody,@nis_active,@ccreated_by,@lcreated_date,@cmodified_by,@lmodified_date );SELECT SCOPE_IDENTITY();";                        
                         using (var cmd = new SqlCommand(queryMaster, conn, transaction))
                         {
                             cmd.Parameters.AddWithValue("@ctenant_id", tenantId);
@@ -51,8 +51,8 @@ namespace TaskEngineAPI.Services
                             cmd.Parameters.AddWithValue("@capi_url", (object?)model.capi_url ?? DBNull.Value);
                             cmd.Parameters.AddWithValue("@capi_params", (object?)model.capi_params ?? DBNull.Value);
                             cmd.Parameters.AddWithValue("@capi_headers", (object?)model.capi_headers ?? DBNull.Value);
-                            cmd.Parameters.AddWithValue("@cbody", (object?)model.@cbody ?? DBNull.Value);
-                            cmd.Parameters.AddWithValue("@nis_active", (object?)model.@nis_active ?? DBNull.Value);
+                            cmd.Parameters.AddWithValue("@cbody", (object?)model.cbody ?? DBNull.Value);
+                            cmd.Parameters.AddWithValue("@nis_active", (object?)model.nis_active ?? DBNull.Value);
                             cmd.Parameters.AddWithValue("@ccreated_by", userName);
                             cmd.Parameters.AddWithValue("@lcreated_date", DateTime.Now);                     
                             cmd.Parameters.AddWithValue("@cmodified_by", userName);
